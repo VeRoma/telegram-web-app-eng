f22cf7ebdbbfaed311fdedfe5f04398751251db7
VeRoma <r.vetokhin@gmail.com>
2024-08-27 01:02:08 +0600

    add buttons

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
 
 
 
@@ -8,12 +9,13 @@ function App() {
 
 	const {onToggleButton, tg} = useTelegram()
 
-	useEffect(() => {
+	useEffect( () => {
 		tg.ready();
 	}, []);
 
 	return (
 		<div className="App">
+			<Header />
 			<button onClick={onToggleButton}>toggle</button>
 			
 		</div>
