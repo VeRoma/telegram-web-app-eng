7d09677a84908ee3ff3cb08a9ef43d5770107f39
Roman Vetokhin <r.vetokhin@gmail.com>
2024-08-27 17:44:53 +0600

    test form

--- front/src/components/Form/Form.js
+++ front/src/components/Form/Form.js
@@ -5,7 +5,8 @@ const Form = (props) => {
 	return (
 		// className={"header"}
 		<div className="buttons-block">
-			<input id className="display" type="text" disabled value="test test test" />
+			
+			<input className="display" type="text" disabled value="test test test" />
 
 			<Button id="option_1" className="neon-button">Option 1</Button>
 			<Button id="option_2" className="neon-button">Option 2</Button>
