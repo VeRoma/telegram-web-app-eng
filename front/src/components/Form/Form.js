590f6f97b606d906c498ebe7acf60c5e90a72e24
Roman Vetokhin <r.vetokhin@gmail.com>
2024-08-27 14:47:12 +0600

    test form

--- front/src/components/Form/Form.js
+++ front/src/components/Form/Form.js
@@ -3,14 +3,14 @@ import React from "react";
 const Form = (props) => {
 	return (
 		// className={"header"}
-		<div className={"buttons-block"}>
+		<div className="buttons-block">
 			<input className={"display"} type="text" disabled />
 
-			<button id="option_1" class="neon-button"></button>
+			<button id="option_1" className="neon-button">option_1</button>
 
-			<button id="option_2" class="neon-button"></button>
+			<button id="option_2" className="neon-button">option_2</button>
 
-			<button id="option_3" class="neon-button"></button>
+			<button id="option_3" className="neon-button">option_3</button>
 		</div>
 	);
 };
